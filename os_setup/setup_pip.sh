a021a1b2efbc8b2fa38426a591d7ff7cea3fe7dc
heyihan <heyihan@didichuxing.com>
2016-05-14 19:52:31 +0800

    keep going

---
+++ os_setup/setup_pip.sh
@@ -0,0 +1,3 @@
+# when python version < 2.7.9
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
---
+++ os_setup/setup_supervisor.sh
@@ -0,0 +1,8 @@
+# install supervisor by easy_install
+easy_install supervisor
+
+# configure
+mv sample.conf /etc/supervisord.conf
+
+#run
+supervisord
